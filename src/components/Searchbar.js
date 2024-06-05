79642cb5d7417bd70f8944fa0ec2d760bf863e22
Carlos Prieto <cmprieto.it@gmail.com>
2024-06-05 17:55:57 +0200

    correccion funcionamiento searchbar->metemos form y tipo de funcionamiento button de button a submit

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,29 +5,23 @@ import lupa from "../assets/icon/buscar.png";
 const Searchbar = () => {
   const { setTermFromSearchBar } = useUserContext();
   const [texto, setTexto] = useState();
-  const busqueda = (e) => {
+  const handleChange = (e) => {
     setTexto(e.target.value);
+    console.log(texto);
   };
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    console.log("texto", texto);
     setTermFromSearchBar(texto);
+    e.preventDefault();
   };
   return (
     <div className="searchbar">
-      {/*  <form className="search" >
-            <input type="search" placeholder="Search here..." required onChange={busqueda}/>
-            <button type="button" onClick={handleSubmit}>Search</button>
-        </form> */}
-      <input
-        type="search"
-        className="searchTerm"
-        placeholder="Buscar"
-        onChange={busqueda}
-      />
-      <button type="button" className="searchButton" onClick={handleSubmit}>
-        <img src={lupa} alt="lupa" className="searchButton--lupa"/>
-
-    {/*    <i className="fa fa-search" aria-hidden="true"></i> */ }
-      </button>
+      <form onSubmit={handleSubmit} className="searchbar--form">
+        <input type="search" className="searchTerm" placeholder="Buscar" onChange={handleChange}/>
+        <button type="submit" className="searchButton">
+          <img src={lupa} alt="lupa" className="searchButton--lupa" />
+        </button>
+      </form>
     </div>
   );
 };
