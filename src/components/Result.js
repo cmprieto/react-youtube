cf1bffa426a3da027faf11bc1d5e035ec00a4d46
Carlos Prieto <cmprieto.it@gmail.com>
2024-07-01 01:26:21 +0200

    mediaQueries nuevas funcionalidades

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,40 +1,38 @@
 import { Fragment } from "react";
-import {useUserContext} from '../providers/UserProvider';
+import { useUserContext } from "../providers/UserProvider";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import { Link } from "react-router-dom";
 import favo from "../assets/icon/fav.png";
 import favSelected from "../assets/icon/fav--selected.png";
-
 
 const Result = ({ videotoShow }) => {
   const [favorito, setFavorito] = useLocalStorage("favoritos-youtube", []);
   const VIDEO_ID = videotoShow.id.videoId;
   const { setChannel } = useUserContext();
   const deleteFavLocalStorage = () => {
-   // alert("BORRO FAVORITO");
+    // alert("BORRO FAVORITO");
     setFavorito(
       favorito.filter((favoritos) => favoritos.id.videoId !== VIDEO_ID)
     );
   };
-//LOGICA PARA MANEJO DE ESTADO (videotoShow.isFavorite) SI UN VIDEO ES FAVORITO, SE AÑADE AL LOCALSTORAGE Y SE QUITA SINO 
+  //LOGICA PARA MANEJO DE ESTADO (videotoShow.isFavorite) SI UN VIDEO ES FAVORITO, SE AÑADE AL LOCALSTORAGE Y SE QUITA SINO
   const handleFav = () => {
     if (videotoShow.isFavorite === false) {
-     // alert("AÑADO FAVORITO");
+      // alert("AÑADO FAVORITO");
       //añadir favorito a localstorage solo si valor de favorito es true
       videotoShow.isFavorite = true;
       setFavorito([...favorito, videotoShow]);
-    }
-    else{
+    } else {
       videotoShow.isFavorite = false;
       deleteFavLocalStorage();
     }
   };
 
-  const handleChannel=()=>{
+  const handleChannel = () => {
     alert(videotoShow.snippet.channelId);
     setChannel(videotoShow.snippet.channelId);
-  }
-  
+  };
+
   return (
     <Fragment>
       <iframe
@@ -49,17 +47,30 @@ const Result = ({ videotoShow }) => {
       <div className="videodetail--videocontainer--subframe">
         <div className="videodetail--videocontainer--subframe--texto">
           <h1>{videotoShow.snippet.title}</h1>
-          <p className="videodetail--videocontainer--subframe--texto--description">{videotoShow.snippet.description}</p>
-         {/*  <p className="videodetail--videocontainer--subframe--texto--time"></p> */}
-          <p className="videodetail--videocontainer--subframe--texto--channel">published by <Link to={`/react-youtube/channel/${videotoShow.snippet.channelId}`} className="videodetail--videocontainer--subframe--texto--channel--link">{videotoShow.snippet.channelTitle}</Link> in {new Date(videotoShow.snippet.publishTime).toLocaleDateString()}</p>
+
+          {/*  <p className="videodetail--videocontainer--subframe--texto--time"></p> */}
+          <p className="videodetail--videocontainer--subframe--texto--channel">
+            published by{" "}
+            <Link
+              to={`/react-youtube/channel/${videotoShow.snippet.channelId}`}
+              className="videodetail--videocontainer--subframe--texto--channel--link"
+            >
+              {videotoShow.snippet.channelTitle}
+            </Link>{" "}
+            in {new Date(videotoShow.snippet.publishTime).toLocaleDateString()}
+          </p>
+          <p className="videodetail--videocontainer--subframe--texto--description">
+            {videotoShow.snippet.description}
+          </p>
         </div>
         {videotoShow.isFavorite ? (
           <img src={favSelected} alt="favorito" onClick={handleFav} />
         ) : (
           <img src={favo} alt="NoFavorito" onClick={handleFav} />
         )}
-      {/*   <button onClick={handleChannel}>videos de canal</button>
-      */} </div>
+        {/*   <button onClick={handleChannel}>videos de canal</button>
+         */}{" "}
+      </div>
     </Fragment>
   );
 };
